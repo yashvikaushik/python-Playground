d54b4c1a313a2c4407e45ba4ead04e7b5edfb839
yashvikaushik <yashvikaushik2525@gmail.com>
2025-08-25 11:41:59 +0530

    basic functionalities
    input() method ,arithmetic operators ,type() method

---
+++ basics/basicFunctionalities.py
@@ -0,0 +1,22 @@
+a=int(input("enter number 1: "))
+b=int(input("enter number 2: "))
+sum=a+b
+print("Number 1: ",a)
+print("Number 2: ",b)
+print("Sum of two numbers is: ",sum)
+remainder=sum%2
+quotient=sum//2
+average=sum/2
+print("average of numbers is: ",average)
+print("Remainder when number is divided by 2: ",remainder)
+print("Quotient when number is divided by 2: ",remainder)
+print("datatype of ramainder: ",type(remainder))
+print("datatype of quotient: ",type(quotient))
+
+c=34
+d=80
+comparision=c>d
+print("is 34 > 80: ",comparision)
+
+num=int(input("Enter the number whose square you want to be calculated: "))
+print("square of the number is: ",num**2)
