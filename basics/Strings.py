9b58170892d3ec4e2f63e618aa19009414c9ee59
yashvikaushik <yashvikaushik2525@gmail.com>
2025-08-26 12:26:16 +0530

    strings covered

---
+++ basics/Strings.py
@@ -0,0 +1,65 @@
+#1.) reversing a string
+# name=input("Enter a string which you want to be reversed \n")
+# revName=name[::-1]
+# print("the string in reverse is: ",revName)
+
+#2.) checking string palindrome
+# name=input("Enter a string which you want to be reversed \n")
+# revName=name[::-1]
+# print("the string in reverse is: ",revName)
+# if(name==revName):
+#     print("palindrome")
+# else:
+#     print("not palindromne")
+
+#3.)Count vowels and consonants
+# cv=0
+# cc=0
+# string=input("enter a string of your choice \n")
+# string.lower()
+# for ch in string:
+#     if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+#         cv+=1
+#     else:
+#         cc+=1
+# print(f"number of constants is {cc}")
+# print(f"number of vowels is {cv}")
+
+#4.) Alternate characters
+# string=input("enter a string of your choice \n")
+# for i in range(0, len(string), 2):
+#     print(string[i])
+
+#5.)Convert a string to uppercase, lowercase, and title case. 
+# string=input("enter a string of your choice \n")
+# print("string in lower case: ",string.lower())
+# print("string in upper case: ",string.upper())
+# print("string in title case: ",string.title())
+
+#6.)Substring check
+# word=input("enter a word: \n")
+# subword=input("enter a word to check if it is in the word entered or not: \n")
+# if(subword in word):
+#     print("yes")
+# else:
+#     print("no")
+
+#7.) Count a specific word
+# s=input("enter a string of your choice \n")
+# w=input("enter a word to count its occurence in the string: \n")
+# count=0
+# words=s.split()
+# for i in words:
+#     if(i==w):
+#         count+=1
+# print(count)
+
+
+
+
+
+
+
+
+
+
