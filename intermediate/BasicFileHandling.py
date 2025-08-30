03901e375145c40e3273b92bb945bb2e3c8072f7
yashvikaushik <yashvikaushik2525@gmail.com>
2025-08-31 02:59:32 +0530

    file handling

---
+++ intermediate/BasicFileHandling.py
@@ -0,0 +1,5 @@
+#basic file handling and crud operations
+r=open("sample.txt",'a') #the w mode creates a file if not present already
+#r.write("hello my name is yashvi and i am writing in this file ")
+#r.write("\n now i am appending some content in this file ")
+print(r)
